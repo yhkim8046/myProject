59b66c09f1ce29c4049ff548d819ac51d8e61e5d
yhkim8046 <yhkim8046@gmail.com>
2024-07-30 22:25:31 +1200

    git conflict

--- my-app/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ my-app/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcb2240f393bfc2890d23c5c22e7187311332505")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+273b48133510d6582846b5085f8c462203021a24")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
